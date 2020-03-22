98dd9e1edc5c51331bec4461301a633bd6cb8786
Aldo Vignone <aldo.vignone@gmail.com>
2020-03-22 19:32:57 -0300

    Set active nav link while scrolling

--- Front-End Web Developer/landing-page/js/app.js
+++ Front-End Web Developer/landing-page/js/app.js
@@ -1,21 +1,21 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 const header = document.querySelector('header.page__header');
 const navList = document.getElementById("navbar__list");
@@ -24,7 +24,7 @@ const sections = document.getElementsByTagName('section');
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 // Get absolute distances of all sections between viewport's top less the height of the navigation bar
 function getAllSectionsTopViewport() {
@@ -39,11 +39,15 @@ function getAllSectionsTopViewport() {
     return ret;
 }
 
+function setNavSectionLinkState(sectionId, setActive) {
+    const navLink = document.getElementById(`${sectionId}-link`);
+    setActive ? navLink.classList.add('menu__active_link') : navLink.classList.remove('menu__active_link');
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 /**
@@ -57,7 +61,8 @@ for (const section of sections) {
     let navListElement = document.createElement('li');
     let navListElementLink = document.createElement('a');
     navListElementLink.className = "menu__link";
-    navListElementLink.setAttribute("href", `#${section.id}`);    
+    navListElementLink.setAttribute("id", `${section.id}-link`);
+    navListElementLink.setAttribute("href", `#${section.id}`);
     navListElementLink.textContent = section.dataset.nav;
     // If scroll to sections is done with events, this attribute is used to know to whick element we must scroll in click
     // event callback
@@ -70,7 +75,7 @@ navList.appendChild(navListContent);
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 // Add class 'active' to section when near top of viewport
 window.addEventListener("scroll", event => {
@@ -80,14 +85,17 @@ window.addEventListener("scroll", event => {
     for(let section of sections) {
         if (index === indexActiveSection) {
             section.classList.add('your-active-class');
+            setNavSectionLinkState(section.id, true);
         } else {
             section.classList.remove('your-active-class');
+            setNavSectionLinkState(section.id, false);
         }
         index++;
     };
 });
 
 /*
+// Nav bar scrolling to section using event
 // Scroll to section on link click
 navList.addEventListener('click', (e) => {
     const clickedNavOption = e.target;
