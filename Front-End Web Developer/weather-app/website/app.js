3fb4866f88660e10e465700f6112a56b2b33487e
Aldo Vignone <aldo.vignone@gmail.com>
2020-04-15 23:50:00 -0300

    Working

--- Front-End Web Developer/weather-app/website/app.js
+++ Front-End Web Developer/weather-app/website/app.js
@@ -10,7 +10,6 @@ const getData = async (url='') => {
     try {
         // Do request
         const data = await response.json();
-        console.log('getData: Data received = ' + JSON.stringify(data));
         return data;
     }
     catch(error) {
@@ -27,9 +26,8 @@ const postData = async ( url = '', data = {}) => {
         },
         body: JSON.stringify(data),       
     });
-    try {
-      const newData = await response.json();
-      return newData
+    try { 
+      return await response.json();
     }
     catch(error) {
         console.log("error", error);
@@ -37,7 +35,7 @@ const postData = async ( url = '', data = {}) => {
 }
 
 
-// https://openweathermap.org/current#{By ZIP CODE}
+// Reference: https://openweathermap.org/current#{By ZIP CODE}
 const getTemperatureByZipCode = async (zipCode) => {
     const weatherData = await getData(`${baseUrl}?zip=${zipCode}&appid=${openWeatherMapAPIKey}`);
     const temp = weatherData.main.temp;
@@ -52,7 +50,18 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 // Add Event Listener to button[id="generate"]
 document.getElementById('generate').addEventListener('click', async (e) => {
     const zipCodeInput =  document.getElementById('zip').value;
-    console.log('ZIPCode = ' + zipCodeInput);
-    const temp = await getTemperatureByZipCode(zipCodeInput);
-    console.log('Temp = ' + temp);
+    getTemperatureByZipCode(zipCodeInput).then((temp) => {
+        const feelingsInput = document.getElementById('feelings').value;
+        const data = {
+            'temperature': temp,
+            'date': newDate,
+            'userResponse': feelingsInput, 
+        };
+        postData('/projectData', data).then(async () => {
+            const data = await getData('/projectData');
+            document.getElementById('date').textContent = `Date: ${data.date}`;
+            document.getElementById('temp').textContent = `Temperature: ${data.temperature}`;
+            document.getElementById('content').textContent = `User Response: ${data.userResponse}`;    
+        })
+    });   
 });
